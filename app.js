143d71166ca0ef80b0fa8348b030cf1cc3965554
steelcm <csteel@gmail.com>
2015-10-18 17:16:52 +0100

    App update for heroku access

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var http = require('http');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -8,7 +9,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 
 var app = express();
-
+app.set('port', process.env.PORT || 3000);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -52,6 +53,10 @@ app.use(function(err, req, res, next) {
     message: err.message,
     error: {}
   });
+});
+
+http.createServer(app).listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
 });
 
 
